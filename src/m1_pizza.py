38b89a4189b99198daa541212e1f9acc544d36ea
saylorsa <saylorsa@rose-hulman.edu>
2019-04-01 10:24:13 -0400

    m1, Done - 9

--- src/m1_pizza.py
+++ src/m1_pizza.py
@@ -455,7 +455,7 @@ def polygon(window, circle, number_of_segments, color, thickness):
 def run_test_fancy_polygon():
     """ Tests the   fancy_polygon   function. """
     # -------------------------------------------------------------------------
-    # TODO: 9. Implement this TEST function.
+    # DONE: 9. Implement this TEST function.
     #   It TESTS the   fancy_polygon   function defined below.
     #   Include at least ** 1 ** ADDITIONAL test (that YOU write).
     #
@@ -503,6 +503,14 @@ def run_test_fancy_polygon():
     #   a contrasting color for the lines makes them especially pretty.
     # -------------------------------------------------------------------------
 
+    title = ('FANCY POLYGON test 4:  400 lime green lines on blue circle,'
+             + ' hops = 33.')
+    window = rg.RoseWindow(480, 350, title)
+
+    circle = rg.Circle(rg.Point(240, 165), 200)
+    circle.fill_color = 'blue'
+    fancy_polygon(window, circle, 400, 33, 'lime green', 5)
+    window.close_on_mouse_click()
 
 def fancy_polygon(window, circle, number_of_lines, hops_to_next_point, color,
                   thickness):
